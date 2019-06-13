77f2c689f9a62db98ad8268c9d5a5a35eaaa0180
ClauGarfiasMx <apps@claudiagarfias.works>
2019-06-13 08:49:20 -0500

    Navigation phase 1 done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Navigation from "../Navigation";
+import LandingPage from "../Landing";
+import SignUpPage from "../SignUp";
+import SignInPage from "../SignIn";
+import PasswordForgetPage from "../PasswordForget";
+import HomePage from "../Home";
+import AccountPage from "../Account";
+import AdminPage from "../Admin";
+import * as ROUTES from "../../constants/routes";
+
 import styled from "styled-components";
 
-const H1sty = styled.h1`
+const H1 = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
@@ -10,7 +21,19 @@ const H1sty = styled.h1`
 const App = () => {
   return (
     <div>
-      <H1sty>App</H1sty>
+      <H1>App</H1>
+      <Router>
+        <div>
+          <Navigation /> <hr />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        </div>
+      </Router>
     </div>
   );
 };
