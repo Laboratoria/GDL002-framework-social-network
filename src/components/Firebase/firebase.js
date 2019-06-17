01dff541c252b1f58f8a6a1f12ee8ad08f62fb01
ClauGarfiasMx <apps@claudiagarfias.works>
2019-06-17 12:21:24 -0500

    firestore added

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,6 @@
-import app from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -9,14 +9,15 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
-    this.auth = app.auth(); // *** Instantiate Firebase AUTH ***
     this.fieldValue = app.firestore.FieldValue;
+    // FIREBASE API'S
+    this.auth = app.auth(); // *** Instantiate Firebase AUTH ***
     this.db = app.firestore();
   }
   // *** Firebase AUTH API ***
@@ -30,8 +31,11 @@ class Firebase {
 
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
-  doPasswordUpdate = password =>
-    this.auth.currentUser.updatePassword(password);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+  user = uid => this.db.ref(`users/${uid}`);
+  users = () => this.db.ref("users");
 }
 
 export default Firebase;
