62a8992ad1e260e026ec28447e1e116320328745
unknown <iaguiarg@hotmail.com>
2019-07-24 11:47:39 -0500

    Modifique titulo de la red social

--- react-front/src/core/Home.js
+++ react-front/src/core/Home.js
@@ -7,7 +7,7 @@ const Home = () => (
   <div className='bckg-home'>
     <div className="jumbotron bckg-home2 bg-white">
      
-      <p className="lead"><h2>Welcome</h2></p>
+      <p className="lead"><h2>Welcome to Ghibli Community</h2></p>
     </div>
     <div className="container">
       <Posts />
